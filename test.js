cbe24af9921163b2cf129c66ee791deba3a9ff86
Mike Della Donna <mpdelladonna@gmail.com>
2012-11-16 18:55:40 -0500

    Added demo files

---
+++ test.js
@@ -0,0 +1,41 @@
+var context;
+var size;
+var gridsize = 50;
+
+function example(border)
+{
+	size = border;
+	var can = document.getElementById('example');
+	context = can.getContext('2d');
+	context.fillStyle = 'green';
+	context.fillRect(0,0,size,size);
+	
+	drawGrid();
+	drawMan();
+	document.addEventListener("click", drawMan, false);
+}
+
+function drawGrid()
+{
+	for(var i = 0; i < size / 50; i++)
+	{
+		context.moveTo(i*gridsize,0);
+		context.lineTo(i*gridsize,size);
+		context.stroke();
+	}
+	
+	for(var i = 0; i < size / 50; i++)
+	{
+		context.moveTo(0,i*gridsize);
+		context.lineTo(size,i*gridsize);
+		context.stroke();
+	}
+	
+}
+
+function drawMan()
+{
+	 var man = new Image();
+	 man.src = 'man.gif';
+	 context.drawImage(man, 0, 0);
+}

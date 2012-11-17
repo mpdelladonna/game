2db20f00f698265de198538109a2685e2f0e4337
Mike Della Donna <mpdelladonna@gmail.com>
2012-11-17 15:07:56 -0500

    added back and forth movement

--- test.js
+++ test.js
@@ -2,18 +2,53 @@ var context;
 var size;
 var gridsize = 50;
 
+var steve;
+
+function loc(x,y)
+{
+	this.x = x;
+	this.y = y;
+}
+
+function man(imgSrc, loc)
+{
+	this.img = new Image();
+	this.img.src = imgSrc;
+	this.pos = loc;
+	this.drawMan = drawMan;
+	this.moveManX = moveManX;
+	this.moveManY = moveManY;
+	
+function drawMan()
+{
+	 context.drawImage(this.img, this.pos.x, this.pos.y);
+}
+
+function moveManX(deltaX)
+{
+	 this.pos.x += deltaX;
+}
+
+function moveManY(deltaY)
+{
+	this.pos.y += deltaY;
+}
+
+}
+
+
 function example(border)
 {
 	size = border;
 	var can = document.getElementById('example');
 	context = can.getContext('2d');
-	context.fillStyle = 'green';
-	context.fillRect(0,0,size,size);
+
+	steve = new man( 'man.gif' ,new loc(0,0));
 	
-	drawGrid();
-	drawMan();
-	document.addEventListener("click", drawMan, false);
-	document.addEventListener("keypress", moveMan, false);
+	document.addEventListener("click", function(event){steve.moveManX(gridsize)}, false);
+	document.addEventListener("keypress", function(event){steve.moveManX(0 - gridsize)}, false);
+	
+	setInterval(update,100);
 }
 
 function drawGrid()
@@ -34,16 +69,9 @@ function drawGrid()
 	
 }
 
-function drawMan()
+function update()
 {
-	 var man = new Image();
-	 man.src = 'man.gif';
-	 context.drawImage(man, 0, 0);
-}
-
-function moveMan()
-{
-	 var man = new Image();
-	 man.src = 'man.gif';
-	 context.drawImage(man, gridsize, 0);
+	context.clearRect(0,0,size,size);
+	drawGrid();
+	steve.drawMan();
 }
